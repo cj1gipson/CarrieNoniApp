ef6fc6d68ea1739c40fd4eb67d7f0d349eeec342
Malabeadman <deangelo.lee19@gmail.com>
2023-01-12 11:43:52 -0500

    sql added but currently broken

--- Carrie Noni/Node/app.js
+++ Carrie Noni/Node/app.js
@@ -9,3 +9,38 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+var mysql = require('mysql');
+
+////////////////////////////////////////////////////////////////////////////////
+//THIS IS THE DATABASE CONNECTION!!!! YOUR MYSQL INFOMATION GOES HERE //////////////
+////////////////////////////////////////////////////////////////////////////////
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "X35!SiCk"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected! to MySql");
+  console.log("Running Query......");
+
+    //////////////////////////////////////////////////////////
+    //Query specific to my table. Change tto fit your table
+    //////////////////////////////////////////////////////////
+    
+    var usesql = "USE DeTest";
+    var sql = "Select * From Testusers";
+    
+    con.query(usesql, function (err, result) {
+        if (err) throw err;
+        console.log("Query Completed Using db:");
+      });
+    
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Query Completed :");
+      });
+});
