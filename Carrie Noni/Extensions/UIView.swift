9b6aaa746a473ec0f054ecbd673e4b09ee0b871c
Malabeadman <deangelo.lee19@gmail.com>
2023-05-10 14:09:41 -0400

    watchList table added still setting upp

---
+++ Carrie Noni/Extensions/UIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView+Ext.swift
+//  Carrie Noni
+//
+//  Created by Deangelo Stewart on 5/10/23.
+//
+
+import SwiftUI
+
+extension UIView{
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+    }
+}
--- Carrie Noni/WatchList.swift
+++ Carrie Noni/WatchList.swift
@@ -2,7 +2,7 @@
 //  LoginView.swift
 //  Carrie Noni
 //
-//  Created by Corey Gipson on 2/2/22.
+//  Created by Corey Gipson & DeAngelo Stewart on 2/2/22.
 //
 
 import Foundation
@@ -10,105 +10,94 @@ import SwiftUI
 import WebKit
 import AVKit
 import UIKit
+//-------------------------- WatchList ------------------------------
 
 struct WatchList: View{
-    
-    @State var index = 0
-    
-    public var WatchListTable: UITableView = {
-       let WatchListTable = UITableView()
-        WatchListTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-       return WatchListTable
-   }()
-    
-    
-    var body: some View {
         
-        ZStack{
-            //------------------------- Background -----------------------------
-            RoundedRectangle(cornerRadius: 25.0)
-                .fill(
-                    RadialGradient(gradient: gradient, center: .topLeading, startRadius: 5, endRadius: 250)
-                )
-                .overlay(Color("Dark-Blue")).opacity(0.6)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+        
+        @State var index = 0
+        
+        public var WatchListTable: UITableView = {
+            let WatchListTable = UITableView()
+            WatchListTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+            return WatchListTable
+        }()
+        
+        
+        var body: some View {
             
-            //-------------------------- Header ------------------------------
-            
-            VStack{
-                HStack{
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    Image("cn")
-                        .imageScale(.large)
-                        .padding(.leading, 65.0)
-                        .font(Font.system(size:45, weight: .heavy))
-                        .foregroundColor(Color("Pink"))
-                        .shadow(color: Color("Pink"), radius: 3)
-                    Spacer()
-                    Spacer()
-                    Button(action: {
-                        
-                    }) {
-                        Image("Menu")
+            ZStack{
+                //------------------------- Background -----------------------------
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(
+                        RadialGradient(gradient: gradient, center: .topLeading, startRadius: 5, endRadius: 250)
+                    )
+                    .overlay(Color("Dark-Blue")).opacity(0.6)
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                //-------------------------- Header ------------------------------
+                
+                VStack{
+                    HStack{
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Image("cn")
                             .imageScale(.large)
-                            .font(Font.system(size:30, weight: .heavy))
-                            .foregroundColor(Color("Faded-Blue"))
-                            .padding(.leading, 30.0)
+                            .padding(.leading, 65.0)
+                            .font(Font.system(size:45, weight: .heavy))
+                            .foregroundColor(Color("Pink"))
+                            .shadow(color: Color("Pink"), radius: 3)
+                        Spacer()
+                        Spacer()
+                        Button(action: {
+                            
+                        }) {
+                            Image("Menu")
+                                .imageScale(.large)
+                                .font(Font.system(size:30, weight: .heavy))
+                                .foregroundColor(Color("Faded-Blue"))
+                                .padding(.leading, 30.0)
+                        }
+                        Spacer()
                     }
                     Spacer()
+
+                    var people = [
+                        Person(givenName: "Juan", familyName: "Chavez", emailAddress: "juanchavez@icloud.com"),
+                        Person(givenName: "Mei", familyName: "Chen", emailAddress: "meichen@icloud.com"),
+                        Person(givenName: "Tom", familyName: "Clark", emailAddress: "tomclark@icloud.com"),
+                        Person(givenName: "Gita", familyName: "Kumar", emailAddress: "gitakumar@icloud.com")
+                    ]
+                    
+                    
+                    if #available(iOS 16.0, *) {
+                        Table(people) {
+                            TableColumn("Given Name", value: \.givenName)
+                            TableColumn("Family Name", value: \.familyName)
+                            TableColumn("E-Mail Address", value: \.emailAddress)
+                        }
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                    
+                    //.padding(.bottom, 10.0)
                 }
-                Spacer()
-
-
                 
-                
-
-                
-                //.padding(.bottom, 10.0)
-            }
-            
-            
-
-            //view.addSubview(WatchListTable)
-            ///WatchListTable.dataSource = self
-            //UINavigationItem.setRightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, //action: #selector(<#T##@objc method#>))
-            
-           
-            //func viewDidLoad(){
-                //super.viewDidLoad()
-                //view.addSubview(<#T##view: UIView##UIView#>)
-              //  WatchListTable.dataSource = self
-            //}
-            
-            
-            
-            
-            ///error Closure containing a declaration cannot be used with result builder 'ViewBuilder'
-            ///
-           
-            
         }
-        
+                
+    }
+            
+}
     
+    struct Person: Identifiable {
+        let givenName: String
+        let familyName: String
+        let emailAddress: String
+        let id = UUID()
+
+        var fullName: String { givenName + " " + familyName }
     }
 
-    ///trying to get  table on the sceen to get a list to display our watch list of videos when selected an confimed
-    
-    //func tableView(_ tablesView: UITableView, numberOfROwsInSection section: Int) -> Int {
-    //    0
-    //}
-    
-    //func viewDidLayoutSubViews(){
-    //super.viewDidLayoutSubViews()
-    //  WatchListTable.frame = self
-    //}
-    
-    //func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
-    //UITableViewCell {
-    //    _ = tableView.dequeueReusableCell(withIdentifier: "cell", for: <#T##IndexPath#>)
-    //}
-    
-}
-
+//-------------------------- WatchList End ------------------------------
