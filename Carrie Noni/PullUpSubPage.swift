52fc026fa49cc2a420296fe1820a01f0f6f92db0
Corey Gipson <cj1gipson@gmail.com>
2023-07-19 11:57:51 -0500

    Changes 7/19/2023

--- Carrie Noni/PullUpSubPage.swift
+++ Carrie Noni/PullUpSubPage.swift
@@ -202,7 +202,7 @@ struct mainFeedPullUpSub: View {
                 }.padding(.bottom, 5.0)
             }
         }
-
+        
         Button("Dismiss") {
             dismiss()
         }
@@ -370,6 +370,57 @@ struct mainFeedNfluenceSub: View {
         
     }
 }
+
+struct tabBarViewsub: View {
+    
+    @State var selectedIndex = 1
+    let icons = ["Juice", "home4", "Nfluence-1"]
+    
+    var body: some View {
+        
+        VStack{
+            
+            ZStack{
+                
+                switch selectedIndex{
+                case 0:
+                    noniJuiceView()
+                case 1:
+                    ContentView()
+                default:
+                    NfluenceView()
+                }
+            }
+            //.padding(.top, -150.0)
+                //.background(Color(CNColor1))
+            HStack(spacing: -9.0){
+                ForEach(0..<3, id: \.self){number in
+                    Spacer()
+                    Button(action:{self.selectedIndex = number}, label: {
+                        
+                    if(selectedIndex == number){
+                        Image(icons[number])
+                            .font(.system(size: 55, weight: .regular, design: .default)).padding().padding(.horizontal, -6.0)
+                            .foregroundColor(selectedIndex == number ? Color("Pink") : Color("Dark-Blue-1"))
+                            .shadow(color: Color("Pink"), radius: 3)
+                    } else{
+                        Image(icons[number])
+                            .font(.system(size: 55, weight: .regular, design: .default)).padding().padding(.horizontal, -6.0)
+                            .foregroundColor(selectedIndex == number ? Color("Pink") : Color("Dark-Blue-1"))
+                    }
+                            
+                    })
+                    Spacer()
+                }
+               
+            }.background(Color("Dark-Blue"))
+            .edgesIgnoringSafeArea(.all)
+            
+        }
+    }
+   
+}
+
 
 struct PullUpSubPage_Previews: PreviewProvider {
     static var previews: some View {
